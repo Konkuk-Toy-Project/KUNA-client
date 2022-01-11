c696c23c64503bc430ed105f47451eb59069ac84
이수동 <tnehd1998@gmail.com>
2022-01-11 17:09:03 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/components/MainTitle/MainTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainTitle = () => {
+  return <h1>Main Page</h1>;
+};
+
+export default MainTitle;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainTitle from "../components/MainTitle/MainTitle";
+
+const MainPage = () => {
+  return <MainTitle />;
+};
+
+export default MainPage;

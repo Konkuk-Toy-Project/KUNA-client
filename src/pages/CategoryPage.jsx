dc6364f3c05e0b08510d81bf76cdfcd55d1d8154
이수동 <tnehd1998@gmail.com>
2022-01-16 19:14:02 +0900

    feat: 카테고리, 찜 페이지도 atom으로 정보 받아오기

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useParams } from "react-router";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 
 import ItemList from "../components/Main/ItemList/ItemList";
 import Title from "../components/Main/Title/Title";
+import { itemState } from "../store/like";
 
 const CategoryPage = () => {
+  const items = useRecoilValue(itemState);
   const { category } = useParams();
 
   const convertEngTitleToKor = (category) => {
@@ -26,9 +29,9 @@ const CategoryPage = () => {
   return (
     <CategoryPageWrapper>
       <Title name={convertEngTitleToKor(category)} />
-      <ItemList listType={"main"} />
-      <ItemList listType={"main"} />
-      <ItemList listType={"main"} />
+      <ItemList listType={"main"} items={items} />
+      <ItemList listType={"main"} items={items} />
+      <ItemList listType={"main"} items={items} />
     </CategoryPageWrapper>
   );
 };
--- src/pages/LikePage.jsx
+++ src/pages/LikePage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import ItemList from "../components/Main/ItemList/ItemList";
 import Title from "../components/Main/Title/Title";
+import { itemState } from "../store/like";
 
 const LikePage = () => {
+  const items = useRecoilValue(itemState);
+
   return (
     <LikePageWrapper>
       <Title name="좋아요" />
-      <ItemList listType={"like"} />
+      <ItemList listType={"like"} items={items} />
     </LikePageWrapper>
   );
 };

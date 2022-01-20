2c178cda7cf0fb4ba8070a4d657f2358deda8c08
최정윤 <99jenny0614@naver.com>
2022-01-20 13:27:08 +0900

    feat: Create item detail page

---
+++ src/pages/ItemDetailPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ImgSection from "../components/ItemDetail/ItemBrief/ImgSection";
+import ItemBriefInfo from "../components/ItemDetail/ItemBrief/ItemBriefInfo";
+
+const ItemDetailPage = () => {
+  return (
+    <div id="itemBriefSection">
+      <ImgSection />
+      <ItemBriefInfo />
+    </div>
+  );
+};
+
+export default ItemDetailPage;

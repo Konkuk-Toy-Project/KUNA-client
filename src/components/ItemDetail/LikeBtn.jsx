6b0117287cb03a583f930463b9935552fcae2c7e
최정윤 <99jenny0614@naver.com>
2022-01-20 10:02:47 +0900

    feat : Make LikeBtn

---
+++ src/components/ItemDetail/LikeBtn.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const LikeBtn = ({ num }) => {
+  const [likesNum, setLikesNum] = useState(num);
+  const [isLiked, setIsLiked] = useState(false);
+  const onClick = () => {
+    isLiked ? setLikesNum((cur) => cur - 1) : setLikesNum((cur) => cur + 1);
+    setIsLiked((cur) => !cur);
+  };
+  return (
+    <>
+      <button onClick={onClick}>
+        {isLiked ? "💘" : "🤍"}
+        {"  "}
+        {likesNum}
+      </button>
+    </>
+  );
+};
+
+LikeBtn.propTypes = { num: PropTypes.number.isRequired };
+
+export default LikeBtn;

f27ae023abc9f5048a6cc81cb264a2b004f35bec
최정윤 <99jenny0614@naver.com>
2022-01-28 22:16:32 +0900

    feat: X 아이콘 컴포넌트화

---
+++ src/components/Icon/IconX.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const IconX = () => {
+  return (
+    <>
+      <FontAwesomeIcon icon={faTimes} className={"styles.icon"} />
+    </>
+  );
+};
+
+export default IconX;

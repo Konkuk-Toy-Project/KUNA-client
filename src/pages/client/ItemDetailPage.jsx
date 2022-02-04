c67f09516c08e3210e5828495c0e725b6c50448a
최정윤 <99jenny0614@naver.com>
2022-02-04 22:30:16 +0900

    feat: 아이템 Detail 페이지 구현 - 샘플데이터로 작동

--- src/pages/client/ItemDetailPage.jsx
+++ src/pages/client/ItemDetailPage.jsx
@@ -1,16 +1,57 @@
-import React from "react";
-import ImgSection from "../components/ItemDetail/ItemBrief/ImgSection";
-import ItemBriefInfo from "../components/ItemDetail/ItemBrief/ItemBriefInfo";
-
-import { useRecoilValue } from "recoil";
-import { ItemBriefImgsState } from "../components/ItemDetail/itemData/itemData";
+import React, { useState } from "react";
+import ItemBrief from "../../components/client/ItemDetail/ItemBrief/ItemBrief";
 
 const ItemDetailPage = () => {
-  const itemImgSrcs = useRecoilValue(ItemBriefImgsState);
+  //sample data
+  const [item, setItem] = useState({
+    itemState: "normality",
+    itemImageUrl: [
+      "https://img.sonyunara.com/files/goods/139373/1637742098_0.jpg",
+      "https://img.sonyunara.com/files/goods/139373/1637652166_2.jpg",
+      "https://img.sonyunara.com/files/goods/139373/1637657482_3.jpg",
+      "https://img.sonyunara.com/files/goods/139373/1637715429_4.jpg",
+      "https://img.sonyunara.com/files/goods/139373/1637740513_16.jpg",
+    ],
+    name: "sts9717 [1+1기획] 맨살에 닿아도 부드러운 핫팩 기모 폴라티 2종",
+    price: 15000,
+    preference: 7,
+    registryDate: 220102,
+    sale: 15,
+    itemId: 121935,
+    categoryName: "shirt",
+    categoryId: 1,
+    option1: [
+      {
+        name: "Size M",
+        option1Id: 18,
+        stock: 16,
+        option2: [],
+      },
+      {
+        name: "Size L",
+        option1Id: 15,
+        stock: 5,
+        option2: [
+          {
+            name: "빨간색",
+            stock: 3,
+            option2Id: 12,
+          },
+          {
+            name: "노란색",
+            stock: 2,
+            option2Id: 10,
+          },
+        ],
+      },
+    ],
+    detailImageUrl: [
+      "https://img.sonyunara.com/files/goodsm/139373/1639466954_7.jpg",
+    ],
+  });
   return (
-    <div id="itemBriefSection">
-      <ImgSection imgsrcs={itemImgSrcs} defaultIdx={0} />
-      <ItemBriefInfo />
+    <div>
+      <ItemBrief itemObj={item} />
     </div>
   );
 };

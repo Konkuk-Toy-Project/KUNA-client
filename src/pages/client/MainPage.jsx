23ceac8a8bb5f9924527e3adec556870176e3be4
이수동 <tnehd1998@gmail.com>
2022-02-04 11:08:05 +0900

    refactor: MainPage 특정 상품 Category 컴포넌트로 분리

--- src/pages/client/MainPage.jsx
+++ src/pages/client/MainPage.jsx
@@ -1,41 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-
-import Title from "../../components/client/Main/Title/Title";
 import { useRecoilValue } from "recoil";
-import ItemList from "../../components/client/Main/ItemList/ItemList";
 import { itemState } from "../../store/client/home";
+import Category from "../../components/client/Main/Category/Category";
 
 const MainPage = () => {
   const items = useRecoilValue(itemState);
 
   return (
     <MainPageWrapper>
-      <Category>
-        <CategoryLink to="outer">
-          <Title name={"아우터"} />
-        </CategoryLink>
-        <ItemList listType={"main"} items={items} />
-      </Category>
-      <Category>
-        <CategoryLink to="top">
-          <Title name={"상의"} />
-        </CategoryLink>
-        <ItemList listType={"main"} items={items} />
-      </Category>
-      <Category>
-        <CategoryLink to="pants">
-          <Title name={"하의"} />
-        </CategoryLink>
-        <ItemList listType={"main"} items={items} />
-      </Category>
-      <Category>
-        <CategoryLink to="shoes">
-          <Title name={"신발"} />
-        </CategoryLink>
-        <ItemList listType={"main"} items={items} />
-      </Category>
+      <Category link="outer" name="아우터" listType="main" items={items} />
+      <Category link="top" name="상의" listType="main" items={items} />
+      <Category link="pants" name="하의" listType="main" items={items} />
+      <Category link="shoes" name="신발" listType="main" items={items} />
     </MainPageWrapper>
   );
 };
@@ -47,13 +24,6 @@ const MainPageWrapper = styled.div`
   align-items: center;
   margin: 2em 0;
   text-align: center;
-`;
-
-const Category = styled.div``;
-
-const CategoryLink = styled(Link)`
-  color: black;
-  text-decoration: none;
 `;
 
 export default MainPage;

60134de901b3bcd7761aff30932b35eaeba167bc
최정윤 <99jenny0614@naver.com>
2022-02-04 22:29:09 +0900

    feat: 아이템 brief 컴포넌트 구현(사진, 이름, 옵션 등이 포함됨)

---
+++ src/components/client/ItemDetail/ItemBrief/ItemBrief.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import ImgSlide from "./ImgSlide";
+import LikeBtn from "./BriefInfoComp/LikeBtn";
+import Option from "./BriefInfoComp/Option";
+import BriefHeader from "./BriefInfoComp/BriefHeader";
+
+const ItemBrief = ({ itemObj }) => {
+  const [item, setItem] = useState(itemObj);
+  const [chosenOpts, setChosenOpts] = useState([]);
+  const onBasketClick = () => {
+    console.log("서버로 chosenOpts 보내기");
+  };
+  const onBuyClick = () => {
+    console.log("Path로? 선택내용 보내기");
+  };
+  console.log(chosenOpts);
+
+  return (
+    <div>
+      <ImgSlide imgsrcs={item.itemImageUrl} defaultIdx={0} />
+
+      <div>
+        <BriefHeader name={item.name} price={item.price} sale={item.sale} />
+        {/* 찜개수+사용자의 찜 내역에 포함 해야함--------------------------------- */}
+        <LikeBtn num={item.preference} />
+        {/* 옵션 컴포넌트*/}
+        <Option item={item} chosen={chosenOpts} setChosen={setChosenOpts} />
+        <div name="submit-btns">
+          <button onClick={onBasketClick}>장바구니</button>
+          <button onClick={onBuyClick}>바로결제</button>
+        </div>
+        <img src={item.infoImg}></img>
+      </div>
+    </div>
+  );
+};
+
+ItemBrief.propTypes = { itemObj: PropTypes.object.isRequired };
+
+export default ItemBrief;

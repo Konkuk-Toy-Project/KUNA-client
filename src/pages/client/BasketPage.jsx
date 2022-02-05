6862c60045383cb6b7a6026e319e8b793c30a4ba
이수동 <tnehd1998@gmail.com>
2022-02-05 17:09:46 +0900

    refactor: BasketPage 금액 계산 부분 리팩토링

--- src/pages/client/BasketPage.jsx
+++ src/pages/client/BasketPage.jsx
@@ -10,6 +10,8 @@ const BasketPage = () => {
   const [items, setItems] = useRecoilState(basketItemState);
   const [totalPrice, setTotalPrice] = useState(0);
   const [postPrice, setPostPrice] = useState(3000);
+  const [withoutDiscountPrice, setWithoutDiscountPrice] = useState(0);
+  const [discountPrice, setDiscountPrice] = useState(0);
 
   const onClickDeleteAll = () => {
     setItems([]);
@@ -20,8 +22,13 @@ const BasketPage = () => {
     items.map((item) => {
       return (total += item.price * item.count);
     });
-    return total;
-  }, [items]);
+    setWithoutDiscountPrice(total);
+    if (withoutDiscountPrice > 50000) {
+      setPostPrice(2000);
+    } else {
+      setPostPrice(3000);
+    }
+  }, [withoutDiscountPrice, items]);
 
   const calculateDiscountPrice = useCallback(() => {
     let total = 0;
@@ -32,19 +39,20 @@ const BasketPage = () => {
           Number(item.discount.substring(0, item.discount.length - 1))) /
         100);
     });
-    return total;
+    setDiscountPrice(total);
   }, [items]);
 
   useEffect(() => {
-    const total = calculateTotalPrice();
-    const discount = calculateDiscountPrice();
-    if (total > 50000) {
-      setPostPrice(2000);
-    } else {
-      setPostPrice(3000);
-    }
-    setTotalPrice(total - discount + postPrice);
-  }, [calculateTotalPrice, calculateDiscountPrice, postPrice]);
+    calculateTotalPrice();
+    calculateDiscountPrice();
+    setTotalPrice(withoutDiscountPrice - discountPrice + postPrice);
+  }, [
+    calculateTotalPrice,
+    calculateDiscountPrice,
+    withoutDiscountPrice,
+    discountPrice,
+    postPrice,
+  ]);
 
   return (
     <BasketPageWrapper>
@@ -52,8 +60,8 @@ const BasketPage = () => {
       <PreviewItemList listType={"basket"} items={items} />
       <button onClick={onClickDeleteAll}>전체 삭제</button>
       <button>결제하기</button>
-      <p>기존 금액 : {calculateTotalPrice()}원</p>
-      <p>할인된 금액 : {calculateDiscountPrice()}원</p>
+      <p>기존 금액 : {withoutDiscountPrice}원</p>
+      <p>할인된 금액 : {discountPrice}원</p>
       <p>배송비 : {postPrice}원</p>
       <p>결제 금액 : {totalPrice}원</p>
     </BasketPageWrapper>

0722835bc6473b4977441cbf93b4499388b29617
최정윤 <99jenny0614@naver.com>
2022-02-05 15:24:08 +0900

    feat: QnA table - 임시 데이터 제거, 날짜 데이터 추가, 페이지변경 컴포넌트추가

--- src/components/client/QnA/QnATable.jsx
+++ src/components/client/QnA/QnATable.jsx
@@ -1,87 +1,19 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import PageChanger from "../../common/PageChanger/PageChanger";
 
 const QNA_NUM_UNIT = 5;
-const PAGE_NUM_UNIT = 5;
-const PAGE_UP = "pageUp";
-const PAGE_DOWN = "pageDown";
-const PAGE_MIN = "pageMax";
-const PAGE_MAX = "pageMin";
 
-const QnATable = () => {
-  const [qnAs, setQnAs] = useState([
-    { title: "건의사항", name: "라이언", isAnswered: false },
-    { title: "제품문제", name: "어피치", isAnswered: true },
-    { title: "카카오톡", name: "제이지", isAnswered: true },
-    { title: "배송문제", name: "튜브", isAnswered: false },
-    { title: "건의사항", name: "라이언", isAnswered: false },
-    { title: "제품문제", name: "어피치", isAnswered: false },
-    { title: "카카오톡", name: "제이지", isAnswered: false },
-    { title: "배송문제", name: "튜브", isAnswered: false },
-    { title: "건의사항", name: "라이언", isAnswered: false },
-    { title: "제품문제", name: "어피치", isAnswered: false },
-    { title: "카카오톡", name: "제이지", isAnswered: false },
-    { title: "배송문제", name: "튜브", isAnswered: false },
-    { title: "건의사항", name: "라이언", isAnswered: false },
-    { title: "제품문제", name: "어피치", isAnswered: false },
-    { title: "카카오톡", name: "제이지", isAnswered: false },
-    { title: "배송문제", name: "튜브", isAnswered: false },
-    { title: "건의사항", name: "라이언", isAnswered: false },
-    { title: "제품문제", name: "어피치", isAnswered: false },
-    { title: "카카오톡", name: "제이지", isAnswered: false },
-    { title: "배송문제", name: "튜브", isAnswered: false },
-    { title: "건의사항", name: "라이언", isAnswered: false },
-    { title: "제품문제", name: "어피치", isAnswered: false },
-    { title: "카카오톡", name: "제이지", isAnswered: false },
-    { title: "배송문제", name: "튜브", isAnswered: false },
-  ]);
-  const [selectablePages, setSelectablePages] = useState([]);
+const QnATable = ({ data }) => {
+  const [qnAs, setQnAs] = useState(data);
   const [curPageNum, setCurPageNum] = useState(1);
   const [totalPageNum, setTotalPageNum] = useState(1);
-  const [isWriter, setIsWriter] = useState(false);
 
-  const onPageBtnClick = (e) => {
-    switch (e.target.id) {
-      case PAGE_UP:
-        if (curPageNum < totalPageNum) setCurPageNum((cur) => cur + 1);
-        break;
-      case PAGE_DOWN:
-        if (curPageNum > 1) setCurPageNum((cur) => cur - 1);
-        break;
-      case PAGE_MAX:
-        setCurPageNum(totalPageNum);
-        break;
-      case PAGE_MIN:
-        setCurPageNum(1);
-        break;
-    }
-  };
-
-  useEffect(() => {
-    setTotalPageNum(parseInt(qnAs.length / PAGE_NUM_UNIT) + 1);
-  }, [qnAs]);
-  useEffect(() => {
-    setSelectablePages(
-      Array.from({ length: totalPageNum }, (_, i) => i + 1).filter(
-        (pageNum) => {
-          const share =
-            curPageNum % PAGE_NUM_UNIT === 0
-              ? parseInt(curPageNum / PAGE_NUM_UNIT) - 1
-              : parseInt(curPageNum / PAGE_NUM_UNIT);
-          return (
-            (pageNum % PAGE_NUM_UNIT === 0 &&
-              parseInt(pageNum / PAGE_NUM_UNIT) === share + 1) ||
-            (pageNum % PAGE_NUM_UNIT !== 0 &&
-              parseInt(pageNum / PAGE_NUM_UNIT) === share)
-          );
-        }
-      )
-    );
-  }, [curPageNum, totalPageNum]);
   return (
     <div>
       <table>
         <colgroup>
+          <col />
           <col />
           <col />
           <col />
@@ -92,6 +24,7 @@ const QnATable = () => {
             <th>번호</th>
             <th>제목</th>
             <th>작성자</th>
+            <th>날짜</th>
             <th>답변상태</th>
           </tr>
         </thead>
@@ -101,13 +34,14 @@ const QnATable = () => {
             idx + 1 <= curPageNum * QNA_NUM_UNIT ? (
               <tr key={"qna_" + idx}>
                 <td>{idx + 1}</td>
-                <td>{q.title}</td>
-                <td>{q.name}</td>
+                <td>{q.secret == true ? "비밀글입니다." : q.title}</td>
+                <td>{q.memberName}</td>
+                <td>{q.registryDate === null ? "" : q.registryDate}</td>
                 <td>
-                  <button disabled={!isWriter}>
-                    {!q.isAnswered
+                  <button disabled={q.title === null || !q.answered}>
+                    {!q.answered
                       ? "답변대기"
-                      : isWriter
+                      : q.title != null
                       ? "답변확인"
                       : "답변완료"}
                   </button>
@@ -118,29 +52,18 @@ const QnATable = () => {
         </tbody>
       </table>
 
-      <div name="pageChanger">
-        <button id={PAGE_MIN} onClick={onPageBtnClick}>
-          ⏪
-        </button>
-        <button id={PAGE_DOWN} onClick={onPageBtnClick}>
-          ◀
-        </button>
-        {selectablePages.map((page) => (
-          <span style={curPageNum === page ? { color: "#ba68c8" } : null}>
-            {page}
-          </span>
-        ))}
-        <button id={PAGE_UP} onClick={onPageBtnClick}>
-          ▶
-        </button>
-        <button id={PAGE_MAX} onClick={onPageBtnClick}>
-          ⏩
-        </button>
-      </div>
+      <PageChanger
+        data={qnAs}
+        curPageNum={curPageNum}
+        setCurPageNum={setCurPageNum}
+        totalPageNum={totalPageNum}
+        setTotalPageNum={setTotalPageNum}
+        pageUnit={5}
+      />
     </div>
   );
 };
 
-QnATable.propTypes = {};
+QnATable.propTypes = { data: PropTypes.array.isRequired };
 
 export default QnATable;

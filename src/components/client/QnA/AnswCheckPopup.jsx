c8487b1b20ea81d8c21ae83d4e3ae2239e028d7c
최정윤 <99jenny0614@naver.com>
2022-02-05 16:20:39 +0900

    test: 샘플 데이터 제거

--- src/components/client/QnA/AnswCheckPopup.jsx
+++ src/components/client/QnA/AnswCheckPopup.jsx
@@ -1,53 +1,51 @@
 import React from "react";
 import IconX from "../Icon/IconX";
 import QnAItemInfo from "./QnAItemInfo";
+import PropTypes from "prop-types";
 
-const AnswCheckPopup = () => {
-  const thumbnail =
-    "https://img.sonyunara.com/files/goods/69048/1611793473_21.jpg";
-  const name = "sbs927 자꾸자꾸 파스텔 스탠다드핏 셔츠 7colors";
-  const writer = "춘식이";
-  const title = "상품에 대해 문의사항이 있습니다.";
-  const question =
-    "질문내용입니다 질문내용입니다 질문내용입니다 질문내용입니다";
-  const answer =
-    "답변내용입니다 답변내용입니다 답변내용입니다 답변내용입니다 답변내용입니다 답변내용입니다 ";
-
+const AnswCheckPopup = ({ qnaData, itemData, setSelAnswIdx }) => {
+  const onClick = () => setSelAnswIdx(null);
   return (
     <div>
       <div>상품 Q&A 답변확인</div>
-      <IconX />
+      <IconX onClick={onClick} />
       <br />
 
-      <QnAItemInfo thumbnail={thumbnail} name={name} />
+      <QnAItemInfo thumbnail={itemData.img} name={itemData.name} />
       <div>
         <div name="Q">
           <div name="questionHead">
             <div name="qnaTitle">
               <label>제목</label>
-              <div>{title}</div>
+              <div>{qnaData.title}</div>
             </div>
             <div name="qnaWriter">
               <label>작성자</label>
-              <div>{writer}</div>
+              <div>{qnaData.memberName}</div>
             </div>
           </div>
 
           <div name="questionMain">
             <label>문의사항</label>
-            <p style={{ whiteSpace: "pre-line" }}>{question}</p>
+            <p style={{ whiteSpace: "pre-line" }}>{qnaData.question}</p>
           </div>
         </div>
 
         <div name="A">
           <label>관리자</label>
           <div>
-            <p style={{ whiteSpace: "pre-line" }}>{answer}</p>
+            <p style={{ whiteSpace: "pre-line" }}>{qnaData.answer}</p>
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+AnswCheckPopup.propTypes = {
+  qnaData: PropTypes.object.isRequired,
+  itemData: PropTypes.object.isRequired,
+  setSelAnswIdx: PropTypes.func.isRequired,
 };
 
 export default AnswCheckPopup;

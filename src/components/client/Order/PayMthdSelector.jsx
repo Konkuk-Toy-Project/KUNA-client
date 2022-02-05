4118cbc417220de8e888f20ad94925cbec773af4
최정윤 <99jenny0614@naver.com>
2022-02-05 22:16:01 +0900

    feat: 주문페이지 - 결제방식 선택기능 컴포넌트화

---
+++ src/components/client/Order/PayMthdSelector.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CREDIT = "credit";
+const BANK_BOOK = "bankbook";
+const PAY_METHOD = "payMethod";
+
+const PayMthdSelector = ({ setPayMethod }) => {
+  const onPayMthdClick = (e) => {
+    setPayMethod({ [PAY_METHOD]: e.target.value });
+  };
+  return (
+    <div>
+      <label>결제 방식</label>
+      <ul>
+        <li>
+          <input
+            type="radio"
+            id={CREDIT}
+            name={PAY_METHOD}
+            value={CREDIT}
+            onClick={onPayMthdClick}
+          />
+          <label htmlFor={CREDIT}>신용카드</label>
+        </li>
+        <li>
+          <input
+            type="radio"
+            id={BANK_BOOK}
+            name={PAY_METHOD}
+            value={BANK_BOOK}
+            onClick={onPayMthdClick}
+          />
+          <label htmlFor={BANK_BOOK}>계좌이체</label>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+PayMthdSelector.propTypes = { setPayMethod: PropTypes.func.isRequired };
+
+export default PayMthdSelector;

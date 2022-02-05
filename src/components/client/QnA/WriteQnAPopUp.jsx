b8382cb4cd4c1339d84f5d5755c5e176a5aee108
최정윤 <99jenny0614@naver.com>
2022-02-05 15:22:41 +0900

    feat: QnA 작성 팝업 - 임시데이터 제거, 버튼에 이벤트리스너 추가

--- src/components/client/QnA/WriteQnAPopUp.jsx
+++ src/components/client/QnA/WriteQnAPopUp.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import IconX from "../Icon/IconX";
 import QnAItemInfo from "./QnAItemInfo";
+import PropTypes from "prop-types";
 
-const WriteQnAPopUp = () => {
-  const thumbnail =
-    "https://img.sonyunara.com/files/goods/69048/1611793473_21.jpg";
-  const name = "sbs927 자꾸자꾸 파스텔 스탠다드핏 셔츠 7colors";
-
+const WriteQnAPopUp = ({ itemData, setQnA, setPopWriteQnA }) => {
   const [isSecret, setIsSecret] = useState(false);
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
@@ -14,18 +11,27 @@ const WriteQnAPopUp = () => {
   const onClick = () => setIsSecret((cur) => !cur);
   const onTitleChange = (e) => setTitle(e.target.value);
   const onTextChange = (e) => setText(e.target.value);
-
-  console.log(isSecret);
-  console.log(title);
-  console.log(text);
+  const onClosePopClick = () => setPopWriteQnA(false);
+  const onSubmitClick = () => {
+    console.log("서버 등록");
+    setPopWriteQnA(false);
+    setQnA((cur) =>
+      cur.concat({
+        question: text,
+        answer: null,
+        registryDate: "질문 일시", //---------------------- 날짜 형식에 맞게 받아오는 걸로 수정
+        memberName: "질문자 이름", // --------------------- 서버에서 이름 받아오는 걸로 수정
+        answered: false,
+        secret: isSecret,
+        title: title,
+      })
+    );
+  };
 
   return (
     <div>
       <div>상품 Q&A 쓰기</div>
-      <IconX />
-      <br />
-
-      <QnAItemInfo thumbnail={thumbnail} name={name} />
+      <QnAItemInfo thumbnail={itemData.img} name={itemData.name} />
 
       <div name="writeSection">
         <label htmlFor="qnaTitle">제목</label>
@@ -54,8 +60,8 @@ const WriteQnAPopUp = () => {
       </div>
 
       <div>
-        <button>확인</button>
-        <button>취소</button>
+        <button onSubmit={onSubmitClick}>확인</button>
+        <button onClick={onClosePopClick}>취소</button>
       </div>
     </div>
   );

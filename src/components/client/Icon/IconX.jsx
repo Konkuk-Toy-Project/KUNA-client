6423db92d6d65eea038dc44904f4b504a782b8f3
최정윤 <99jenny0614@naver.com>
2022-02-05 16:20:04 +0900

    feat: X 아이콘에 onClick Property 추가

--- src/components/client/Icon/IconX.jsx
+++ src/components/client/Icon/IconX.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const IconX = () => {
+const IconX = ({ onClick }) => {
   return (
     <>
-      <FontAwesomeIcon icon={faTimes} className={"styles.icon"} />
+      <FontAwesomeIcon
+        icon={faTimes}
+        className={"styles.icon"}
+        onClick={onClick}
+      />
     </>
   );
 };

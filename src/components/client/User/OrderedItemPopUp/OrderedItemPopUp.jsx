d0229937ce83c3745b9c277f454b64b49cf9f710
이수동 <tnehd1998@gmail.com>
2022-02-06 09:53:09 +0900

    refactor: ProfileBanner 관련 컴포넌트 리팩토링

--- src/components/client/User/OrderedItemPopUp/OrderedItemPopUp.jsx
+++ src/components/client/User/OrderedItemPopUp/OrderedItemPopUp.jsx
@@ -10,30 +10,6 @@ import {
 import { currentY } from "../../../../store/common/user";
 import CloseButton from "../../../common/CloseButton/CloseButton";
 
-const OrderedItemPopUpWrapper = styled.div`
-  top: ${(props) => props.top + "px"};
-  left: 10vw;
-  width: 80vw;
-  height: 60vh;
-  border: 1px solid black;
-  background-color: white;
-  position: absolute;
-  border-radius: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const OrderedItemWrapper = styled.div`
-  width: 80%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1em;
-  border: 1px solid black;
-`;
-
 const OrderedItemPopUp = () => {
   const setShowOrderedItemPopUp = useSetRecoilState(showOrderedItemState);
   const orderedItem = useRecoilValue(orderedItemState);
@@ -66,5 +42,29 @@ const OrderedItemPopUp = () => {
     </OrderedItemPopUpWrapper>
   );
 };
+
+const OrderedItemPopUpWrapper = styled.div`
+  top: ${(props) => props.top + "px"};
+  left: 10vw;
+  width: 80vw;
+  height: 60vh;
+  border: 1px solid black;
+  background-color: white;
+  position: absolute;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const OrderedItemWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+  border: 1px solid black;
+`;
 
 export default OrderedItemPopUp;
--- src/components/client/User/RankPopUp/RankPopUp.jsx
+++ src/components/client/User/RankPopUp/RankPopUp.jsx
@@ -5,21 +5,6 @@ import { showRankState } from "../../../../store/client/user";
 import { currentY } from "../../../../store/common/user";
 import CloseButton from "../../../common/CloseButton/CloseButton";
 
-const RankPopUpWrapper = styled.div`
-  top: ${(props) => props.top + "px"};
-  left: 30vw;
-  width: 40vw;
-  height: 30vh;
-  border: 1px solid black;
-  background-color: white;
-  position: absolute;
-  border-radius: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
 const RankPopUp = () => {
   const scrollY = useRecoilValue(currentY);
   const setShowRank = useSetRecoilState(showRankState);
@@ -34,5 +19,20 @@ const RankPopUp = () => {
     </RankPopUpWrapper>
   );
 };
+
+const RankPopUpWrapper = styled.div`
+  top: ${(props) => props.top + "px"};
+  left: 30vw;
+  width: 40vw;
+  height: 30vh;
+  border: 1px solid black;
+  background-color: white;
+  position: absolute;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default RankPopUp;

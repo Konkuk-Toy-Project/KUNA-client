04a81a3ca7f4b61ec6efaa593e1ac049741e70c6
최정윤 <99jenny0614@naver.com>
2022-02-11 03:40:42 +0900

    feat: 회원가입 완료 페이지 구현

--- src/pages/client/SignUpFinPage.jsx
+++ src/pages/client/SignUpFinPage.jsx
@@ -1,7 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const SignUpFinPage = () => {
-  return <div>회원가입완료</div>;
+  const navigate = useNavigate();
+  const onClickToMain = () => navigate("/");
+  const onClickToLogin = () => navigate("/login");
+  return (
+    <div>
+      <div>
+        <FontAwesomeIcon icon={faCircleCheck} style={{ fontSize: 80 }} />
+      </div>
+      <p>회원가입이 완료되었습니다.</p>
+      <ul>
+        <li>
+          <button onClick={onClickToMain}>메인페이지</button>
+        </li>
+        <li>
+          <button onClick={onClickToLogin}>로그인하기</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SignUpFinPage;

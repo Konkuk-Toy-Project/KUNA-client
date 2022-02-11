833a2b3cfc1b49fb1a72f8b112b97712feda1bda
이수동 <tnehd1998@gmail.com>
2022-02-11 11:06:42 +0900

    fix: 서버 연결용 임시 데이터 주석처리

--- src/pages/client/MainPage.jsx
+++ src/pages/client/MainPage.jsx
@@ -2,33 +2,32 @@ import React from "react";
 import styled from "styled-components";
 import Category from "../../components/client/Main/Category/Category";
 import { Suspense } from "react";
-import { useEffect } from "react/cjs/react.development";
-import axios from "axios";
-import { useSetRecoilState } from "recoil";
-import { isClientState, userTokenState } from "../../store/common/user";
+//import axios from "axios";
+//import { useSetRecoilState } from "recoil";
+//import { isClientState, userTokenState } from "../../store/common/user";
 
 const MainPage = () => {
-  const setUserToken = useSetRecoilState(userTokenState);
-  const setIsClient = useSetRecoilState(isClientState);
+  //const setUserToken = useSetRecoilState(userTokenState);
+  //const setIsClient = useSetRecoilState(isClientState);
 
   // 임시 로그인 기능
-  useEffect(() => {
-    async function getData() {
-      const data = await axios({
-        method: "post",
-        url: "http://localhost:8080/member/login",
-        data: {
-          email: "asdf@asdf.com",
-          password: "asdfasdf@1",
-        },
-      }).then((response) => response.data);
-      setUserToken(data);
-      if (data.role !== "user") {
-        setIsClient(false);
-      }
-    }
-    getData();
-  }, [setUserToken, setIsClient]);
+  // useEffect(() => {
+  //   async function getData() {
+  //     const data = await axios({
+  //       method: "post",
+  //       url: "http://localhost:8080/member/login",
+  //       data: {
+  //         email: "asdf@asdf.com",
+  //         password: "asdfasdf@1",
+  //       },
+  //     }).then((response) => response.data);
+  //     setUserToken(data);
+  //     if (data.role !== "user") {
+  //       setIsClient(false);
+  //     }
+  //   }
+  //   getData();
+  // }, [setUserToken, setIsClient]);
 
   return (
     <MainPageWrapper>

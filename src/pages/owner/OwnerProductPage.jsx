93a2fb134d448ddf85968d823e06ce98383bc355
이수동 <tnehd1998@gmail.com>
2022-02-11 10:25:53 +0900

    feat: 관리자 페이지 등록한 상품 나열하기

--- src/pages/owner/OwnerProductPage.jsx
+++ src/pages/owner/OwnerProductPage.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import styled from "styled-components";
 import AddItemPopUp from "../../components/owner/Product/AddItemPopUp/AddItemPopUp";
 import EditItemPopUp from "../../components/owner/Product/EditItemPopUp/EditItemPopUp";
 import EnrolledItemList from "../../components/owner/Product/EnrolledItemList/EnrolledItemList";
+import { userTokenState } from "../../store/common/user";
 import {
   productState,
   showAddPopUpState,
@@ -11,13 +13,26 @@ import {
 } from "../../store/owner/product";
 
 const OwnerProductPage = () => {
-  const items = useRecoilValue(productState);
+  const [items, setItems] = useRecoilState(productState);
   const showEditPopUp = useRecoilValue(showEditPopUpState);
   const [showAddPopUp, setShowAddPopUp] = useRecoilState(showAddPopUpState);
+  const userToken = useRecoilValue(userTokenState);
 
   const onClickAddItem = () => {
     setShowAddPopUp(true);
   };
+
+  useEffect(() => {
+    async function getData() {
+      const data = await axios
+        .get("http://localhost:8080/admin/items", {
+          headers: { Authorization: `Bearer ${userToken.token}` },
+        })
+        .then((response) => response.data);
+      setItems(data);
+    }
+    getData();
+  }, [userToken.token, setItems]);
 
   return (
     <OwnerProductPageWrapper>

857b45f729380e8ee17b021c5676343c1a921c37
최정윤 <99jenny0614@naver.com>
2022-02-12 01:46:26 +0900

    feat: likeBtn 서버 연결

--- src/components/client/ItemDetail/ItemBrief/BriefInfoComp/LikeBtn.jsx
+++ src/components/client/ItemDetail/ItemBrief/BriefInfoComp/LikeBtn.jsx
@@ -1,16 +1,71 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
-const LikeBtn = ({ num }) => {
+// 로그인했는지 아닌지 판단 how? - 에러코드 보내달라고 하기
+const LikeBtn = ({ itemId, num }) => {
   const [likesNum, setLikesNum] = useState(num);
+  const [likeId, setLikeId] = useState();
   const [isLiked, setIsLiked] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const postLike = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post("http://localhost:8080/preference", {
+        itemId: itemId,
+      });
+      setLikeId(response.data.preferenceId);
+      setLoading(false);
+      return;
+    } catch (error) {
+      if (error.response) {
+        const message = error.response.message;
+        if (message !== undefined) {
+          alert(error.response.message);
+          setLoading(false);
+          return;
+        }
+      }
+      alert("오류가 발생했습니다. 다시 한번 시도해주세요.");
+      setLoading(false);
+    }
+  };
+
+  const deleteLike = async () => {
+    setLoading(true);
+    try {
+      await axios.delete(`http://localhost:8080/preference/${likeId}`);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      if (error.response) {
+        const message = error.response.message;
+        if (message !== undefined) {
+          alert(error.response.message);
+          setLoading(false);
+          return;
+        }
+      }
+      alert("오류가 발생했습니다. 다시 한번 시도해주세요.");
+      setLoading(false);
+    }
+  };
+
   const onClick = () => {
-    isLiked ? setLikesNum((cur) => cur - 1) : setLikesNum((cur) => cur + 1);
+    if (isLiked) {
+      setLikesNum((cur) => cur - 1);
+      deleteLike();
+    } else {
+      setLikesNum((cur) => cur + 1);
+      postLike();
+    }
     setIsLiked((cur) => !cur);
   };
+
   return (
     <>
-      <button onClick={onClick}>
+      <button onClick={onClick} disabled={loading}>
         {isLiked ? "💘" : "🤍"}
         {"  "}
         {likesNum}

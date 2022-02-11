1bc2b18d7750ff1a655a2ba50ad12abdf71568ae
최정윤 <99jenny0614@naver.com>
2022-02-12 02:47:27 +0900

    feat: 서버 연결 & 리뷰 페이지 넘기는 컴포넌트 추가

--- src/pages/client/ReviewPage.jsx
+++ src/pages/client/ReviewPage.jsx
@@ -2,62 +2,66 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import ReviewPopup from "../../components/client/Review/ReviewPopup";
 import Review from "../../components/client/Review/Review";
+import axios from "axios";
+import PageChanger from "../../components/common/PageChanger/PageChanger";
+
+const PAGE_UNIT = 5;
+const REVIEW_NUM_UNIT = 5;
 
 const ReviewPage = ({ itemId }) => {
-  // sample data
-  const [reviews, setReviews] = useState([
-    {
-      memberName: "김냥냥",
-      option: "Size L, 빨강",
-      rate: 3.5,
-      description: "상품리뷰입니다 울랄라",
-      registryDate: "22년 2월 5일",
-      reviewImagesUrl: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPU5ILakhRz1Sxs57QLxGFBlsXA1TgOczVFg&usqp=CAU",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYBts2GJsjEOpSL03rk_3oLacW28UZ_oIVzw&usqp=CAU",
-      ],
-    },
-    {
-      memberName: "박동동",
-      option: "Size M, 파랑",
-      rate: 5,
-      description: "별이 다섯개 ",
-      registryDate: "22년 2월 2일",
-      reviewImagesUrl: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYBts2GJsjEOpSL03rk_3oLacW28UZ_oIVzw&usqp=CAU",
-      ],
-    },
-    {
-      memberName: "나무",
-      option: "프리사이즈",
-      rate: 2,
-      description: "별 두개",
-      registryDate: "22년 1월 9일",
-      reviewImagesUrl: [],
-    },
-  ]);
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [curPageNum, setCurPageNum] = useState(1);
+  const [totalPageNum, setTotalPageNum] = useState();
 
   const [popUpReview, setPopUpReview] = useState({}); // 클릭한 아이템의 review + default인덱스
-  useEffect(() => {
-    // 아이템 아이디로 리뷰 배열 받아오기
-    console.log("배열로 받아오기");
+  useEffect(async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/review/${itemId}`
+      );
+      console.log(response.data);
+      setReviews(response.data);
+    } catch (error) {
+      alert(error.response.message);
+      console.log(error);
+    }
+    setLoading(false);
   }, []);
 
   return (
     <div>
-      {reviews.map((review, idx) => (
-        <div key={"rev_" + idx}>
-          <Review reviewObj={review} setPopup={setPopUpReview} />
-          <br />
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {reviews.map((review, idx) =>
+            idx + 1 >= (curPageNum - 1) * REVIEW_NUM_UNIT + 1 &&
+            idx + 1 <= curPageNum * REVIEW_NUM_UNIT ? (
+              <div key={"rev_" + idx}>
+                <Review reviewObj={review} setPopup={setPopUpReview} />
+                <br />
+              </div>
+            ) : null
+          )}
+          <PageChanger
+            data={reviews}
+            curPageNum={curPageNum}
+            setCurPageNum={setCurPageNum}
+            totalPageNum={totalPageNum}
+            setTotalPageNum={setTotalPageNum}
+            pageUnit={PAGE_UNIT}
+          />
         </div>
-      ))}
+      )}
       <ReviewPopup data={popUpReview} setData={setPopUpReview} />
     </div>
   );
 };
 
 ReviewPage.propTypes = {
-  itemId: PropTypes.string.isRequired,
+  itemId: PropTypes.number.isRequired,
 };
 
 export default ReviewPage;

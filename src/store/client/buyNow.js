b65c4142880a48ad780aa521ab7f12608b6df14a
최정윤 <99jenny0614@naver.com>
2022-02-13 10:38:16 +0900

    feat: 바로구매 atom 생성

---
+++ src/store/client/buyNow.js
@@ -0,0 +1,16 @@
+import { atom } from "recoil";
+
+export const BuyNowState = atom([]);
+// {
+//     itemImageUrl: 상품 이미지 저장 이름,
+//     name: 아이템 이름,
+//     price: 판매 가격,
+//     sale: 세일 % 지수,
+//     itemId: 상품 고유 id,
+//     option1Name: 옵션1 이름,
+//     option1Id: 옵션1 고유 id,
+//     option2Name: 옵션2 이름,
+//     option2Id: 옵션2 고유 id,
+//     count: 구매개수,
+//     stock: 선택한 옵션조합의 재고,
+// }

5b4bce1f8d99d4d21d4523aef29eeec33eadb69e
최정윤 <99jenny0614@naver.com>
2022-02-17 00:22:59 +0900

    feat: 로그인여부에 따라 장바구니/바로결제 가능 여부 변경

--- src/components/client/ItemDetail/ItemBrief/ItemBrief.jsx
+++ src/components/client/ItemDetail/ItemBrief/ItemBrief.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import ImgSlide from "../../../common/ImgSlide/ImgSlide";
 import LikeBtn from "./BriefInfoComp/LikeBtn";
@@ -6,23 +6,24 @@ import Option from "./BriefInfoComp/Option";
 import BriefHeader from "./BriefInfoComp/BriefHeader";
 import axios from "axios";
 import AskGoToBasketPopup from "./AskGoToBasketPopup";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { buyingState } from "../../../../store/client/buying";
+import { userTokenState } from "../../../../store/common/user";
 import { useNavigate } from "react-router-dom";
 
 const ItemBrief = ({ itemObj }) => {
   const [item, setItem] = useState(itemObj);
+  const userToken = useRecoilValue(userTokenState);
   const [chosenOpts, setChosenOpts] = useState([]);
   const [chosenOptsSubInfo, setChosenOptsSubInfo] = useState([]);
   const [openBasketPopup, setOpenBasketPopup] = useState(false);
   const [buying, setBuying] = useRecoilState(buyingState);
+  const [isLogin, setIsLogin] = useState(false);
   const navigate = useNavigate();
 
   const onBuyClick = () => {
-    if (chosenOpts.length === 0) {
-      alert("상품을 선택해 주세요.");
-      return;
-    }
+    if (!canDoBtnClickEvent()) return;
+
     setBuying(
       chosenOpts.map((chosen, idx) => {
         return {
@@ -37,24 +38,52 @@ const ItemBrief = ({ itemObj }) => {
     );
   };
 
+  useEffect(async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/member/isLogin`, {
+        headers: { Authorization: `Bearer ${userToken}` },
+      });
+      setIsLogin(response.data.isLogin);
+      return;
+    } catch (error) {
+      if (error.response) {
+        const message = error.response.message;
+        if (message !== undefined) {
+          alert(error.response.message);
+          return;
+        }
+      }
+      alert("오류가 발생했습니다. 다시 한번 시도해주세요.");
+    }
+  }, []);
+
   useEffect(() => {
     if (buying.length === 0) return;
     navigate("/order");
   }, [buying]);
 
   const onBasketClick = () => {
-    if (chosenOpts.length === 0) {
-      alert("상품을 선택해 주세요.");
-      return;
-    }
+    if (!canDoBtnClickEvent()) return;
     postChosenOpt();
   };
+
+  const canDoBtnClickEvent = useCallback(() => {
+    if (chosenOpts.length === 0) {
+      alert("상품을 선택해 주세요.");
+      return false;
+    } else if (!isLogin) {
+      alert("로그인 후 이용해주세요.");
+      return false;
+    }
+    return true;
+  }, [chosenOpts, isLogin]);
 
   const postChosenOpt = async () => {
     try {
       const response = await axios.post(
         "http://localhost:8080/cart",
-        chosenOpts
+        chosenOpts,
+        { headers: { Authorization: `Bearer ${userToken}` } }
       );
       if (response.data !== undefined) setOpenBasketPopup(true);
     } catch (error) {

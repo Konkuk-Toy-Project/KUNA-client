4feece0afae6f20d0473372339c89453e5862227
최정윤 <99jenny0614@naver.com>
2022-02-16 20:22:06 +0900

    feat: 포인트 서버에서 받아오기 + '사용가능한 포인트'추가

--- src/components/client/Order/UsingPoint.jsx
+++ src/components/client/Order/UsingPoint.jsx
@@ -1,35 +1,57 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
-const UsingPoint = ({ setUsePoint }) => {
-  const [ownPoint, setOwnPoint] = useState(1000);
-  const [leftPoint, setLeftPoint] = useState(0);
+const UsingPoint = ({ salePrice, couponSale, setUsePoint }) => {
+  const [ownPoint, setOwnPoint] = useState(0);
   const [inputPoint, setInputPoint] = useState(0);
+  const [availPoint, setAvailPoint] = useState(0);
 
   const onChange = (e) => {
-    const value = e.target.value;
-    if (value > ownPoint) setInputPoint(ownPoint);
-    else if (value < 0) setInputPoint(0);
-    else setInputPoint(value);
+    const value = e.target.value.toString().replace(/[^0-9]/gi, "");
+    const point = value == "" ? 0 : parseInt(value);
+
+    if (point > availPoint) setInputPoint(availPoint);
+    // else if (point < 0) setInputPoint(0);
+    else setInputPoint(point);
   };
 
-  const onClick = () => setInputPoint(ownPoint);
+  const onClick = () => setInputPoint(availPoint);
 
-  useEffect(() => {
-    console.log("보유포인트 받아오기");
+  useEffect(async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/member/point");
+      const point = response.data.point;
+      setOwnPoint(point);
+      setAvailPoint(
+        salePrice - couponSale >= point ? point : salePrice - couponSale
+      );
+    } catch (error) {
+      const response = error.response;
+      alert(
+        response && response.errorCode !== undefined
+          ? response.message
+          : "오류가 발생하였습니다. 다시 시도해주세요"
+      );
+      // window.location.reload();
+    }
   }, []);
 
   useEffect(() => {
-    setLeftPoint(ownPoint - inputPoint);
-    setUsePoint({ ["usePoint"]: inputPoint }); // 주문페이지로 보내는 데이터
+    setUsePoint(parseInt(inputPoint)); // 주문페이지로 보내는 데이터
   }, [inputPoint]);
+
+  console.log(inputPoint);
+  console.log(availPoint);
+  console.log(ownPoint);
 
   return (
     <div>
-      <label>적립금</label>
-      <input type="number" onChange={onChange} value={inputPoint}></input>
+      <label>포인트</label>
+      <input type="text" onChange={onChange} value={inputPoint}></input>
       <button onClick={onClick}>전액사용</button>
-      <span>보유 적립금 {leftPoint}원</span>
+      <span>사용가능 포인트 {availPoint - parseInt(inputPoint)}원</span>
+      <span>보유 포인트 {ownPoint - parseInt(inputPoint)}원</span>
     </div>
   );
 };

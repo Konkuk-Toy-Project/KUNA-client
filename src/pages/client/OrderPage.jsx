55fb8b4020b4b9154eeb1202c3ea5a8141a85194
최정윤 <99jenny0614@naver.com>
2022-02-16 20:22:51 +0900

    feat: 서버로 데이터 전송

--- src/pages/client/OrderPage.jsx
+++ src/pages/client/OrderPage.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import CouponSelector from "../../components/client/Order/CouponSelector.jsx";
@@ -9,48 +10,40 @@ import PriceBar from "../../components/client/Order/PriceBar.jsx";
 import UsingPoint from "../../components/client/Order/UsingPoint.jsx";
 import { buyingState } from "../../store/client/buying.js";
 
+const SHIPPING = 3000;
+const SHIP_FREE_PRICE = 50000;
+
 const OrderPage = () => {
   const navigate = useNavigate();
 
-  const [defaultPrice, setDefaultPrice] = useState(30000);
-  const [saledPrice, setSaledPrice] = useState(defaultPrice);
-  const [additonalSale, setAdditonalSale] = useState({ coupon: 0, point: 0 });
-  const [shippingCharge, setShippingCharge] = useState(3000);
+  const [defaultPrice, setDefaultPrice] = useState(0);
+  const [salePrice, setSalePrice] = useState(defaultPrice);
+  const [couponSale, setCouponSale] = useState(0);
+  const [shippingCharge, setShippingCharge] = useState(SHIPPING);
+
   const [totalPrice, setTotalPrice] = useState(defaultPrice);
 
   const [inputData, setInputData] = useState({});
-  const [couponId, setCouponId] = useState({ couponId: "" });
-  const [usePoint, setUsePoint] = useState({ usePoint: 0 });
-  const [payMethod, setPayMethod] = useState({ payMethod: "" });
+  const [couponId, setCouponId] = useState();
+  const [usePoint, setUsePoint] = useState(0);
+  const [payMethod, setPayMethod] = useState();
 
   const [isInputFilled, setIsInputFilled] = useState(false);
   const [isPayMthdChecked, setIsPayMthdChecked] = useState(false);
 
   const buying = useRecoilValue(buyingState);
-  const navigate = useNavigate();
-
-  const [data, setData] = useState({
-    address: "", //배송지 주소
-    recipient: "", //수령인
-    phone: "", //수령인 전화번호
-    payMethod: "", //결제 방법(card, bankbook)
-    usePoint: "", //포인트 사용액
-    totalPrice: 0, //전체 주문 금액(택배비 미포함)
-    shippingCharge: 0, //택배비
-    couponId: "", //쿠폰 사용 Id
-    orderItems: [],
-    //주문 상품들
-    // itemId: "", //상품 고유 Id
-    // option1Id: "", //옵션1 고유 id
-    // option2Id: "", //옵션2 고유 id
-    // count: "", //구매 개수
-  });
 
   useEffect(() => {
     if (buying.length === 0) navigate("/");
-
-    console.log("input관련 정보들 받아오기(주소 등))"); // => input 관련 컴포넌트로 넘겨주기
   }, [buying]);
+
+  useEffect(
+    () => setTotalPrice(salePrice - couponSale),
+    [defaultPrice, salePrice, couponSale, usePoint]
+  );
+  useEffect(() => {
+    setShippingCharge(totalPrice >= SHIP_FREE_PRICE ? 0 : SHIPPING);
+  }, [totalPrice]);
 
   const onPayBtnClick = () => {
     if (!isInputFilled || !isPayMthdChecked) {
@@ -61,16 +54,34 @@ const OrderPage = () => {
       );
       return;
     }
-
-    setData({
+    postOrder();
+  };
+  const postOrder = useCallback(async () => {
+    console.log({
       ...inputData,
-      ...payMethod,
-      ...usePoint,
-      ...couponId,
+      payMethod: payMethod,
+      usePoint: usePoint,
+      couponId: couponId === undefined ? "" : couponId,
       totalPrice: totalPrice, //전체 주문 금액(택배비 미포함)
       shippingCharge: shippingCharge, //택배비
-      orderItems: [
-        buying.map((item) => {
+      orderItems: buying.map((item) => {
+        return {
+          itemId: item.itemId,
+          option1Id: item.option1Id,
+          option2Id: item.option2Id,
+          count: item.count,
+        };
+      }),
+    });
+    try {
+      const response = await axios.post("http://localhost:8080/order", {
+        ...inputData,
+        payMethod: payMethod,
+        usePoint: usePoint,
+        couponId: couponId === undefined ? "" : couponId,
+        totalPrice: totalPrice, //전체 주문 금액(택배비 미포함)
+        shippingCharge: shippingCharge, //택배비
+        orderItems: buying.map((item) => {
           return {
             itemId: item.itemId,
             option1Id: item.option1Id,
@@ -78,30 +89,34 @@ const OrderPage = () => {
             count: item.count,
           };
         }),
-      ],
-    });
-  };
-  useEffect(async () => {
-    try {
-      const response = await axios.post("http://localhost:8080/order", data);
+      });
       console.log(response.data);
       //navigate(`order/result?쿼리 스트링 전달하기`);
     } catch (error) {
-      const response = error.respose;
+      const response = error.response;
+      console.log(error.response.errorCode);
       alert(
-        respose && response.errorCode !== undefined
+        response && response.errorCode !== undefined
           ? response.message
           : "오류가 발생하였습니다. 다시 시도해주세요"
       );
-      window.location.reload();
+      // window.location.reload();
     }
-  }, [data]);
+  }, [
+    buying,
+    totalPrice,
+    inputData,
+    couponId,
+    shippingCharge,
+    usePoint,
+    payMethod,
+  ]);
 
   // console.log(inputData);
   // console.log(couponId);
   // console.log(payMethod);
   // console.log(items);
-  console.log(data);
+  ///console.log(data);
 
   return (
     <div>
@@ -116,22 +131,29 @@ const OrderPage = () => {
     */}
       <OrderedItems
         setDefaultPrice={setDefaultPrice}
-        setTotalPrice={setTotalPrice}
+        setSalePrice={setSalePrice}
       />
       <PriceBar
         defaultPrice={defaultPrice}
+        salePrice={salePrice}
+        couponSale={couponSale}
+        point={usePoint}
         totalPrice={totalPrice}
         shippingCharge={shippingCharge}
       />
 
       <OrderWriteInfo setData={setInputData} setIsFilled={setIsInputFilled} />
       <CouponSelector
-        totalPrice={totalPrice}
-        defaultPrice={defaultPrice}
-        setTotalPrice={setTotalPrice}
+        salePrice={salePrice}
+        couponSale={couponSale}
+        setCouponSale={setCouponSale}
         setCouponId={setCouponId}
       />
-      <UsingPoint setUsePoint={setUsePoint} />
+      <UsingPoint
+        salePrice={salePrice}
+        couponSale={couponSale}
+        setUsePoint={setUsePoint}
+      />
       <PayMthdSelector
         setPayMethod={setPayMethod}
         setIsChecked={setIsPayMthdChecked}

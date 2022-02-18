ee5825c119967679b7b9ee7f36d6f2d9ed341af4
최정윤 <99jenny0614@naver.com>
2022-02-18 12:02:39 +0900

    feat: 아이디 비밀번호 찾기 css - 팝업css 제외

--- src/pages/client/FindAccountPage.jsx
+++ src/pages/client/FindAccountPage.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import PhoneInput from "../../components/client/Login/PhoneInput";
-import axios from "axios";
+import PageTitle from "../../components/common/PageTitle/PageTitle";
 import IconX from "../../components/client/Icon/IconX";
+import axios from "axios";
+import styled from "styled-components";
 
 const FIND_ID = "findId";
 const FIND_PW = "findPw";
@@ -102,42 +104,52 @@ const FindAccountPage = () => {
   console.log(tempPW);
 
   return (
-    <div>
+    <FindAccountWrapper>
+      <PageTitle title={"아이디·비밀번호 찾기"} />
       {loading ? <div>Loading...</div> : null}
-      <div id="tab-container"></div>
-      <ul>
-        <li id={FIND_ID} onClick={onTabClick}>
-          아이디 찾기
-        </li>
-        <li id={FIND_PW} onClick={onTabClick}>
-          비밀번호 찾기
-        </li>
-      </ul>
+      {/* <div id="tab-container"></div> */}
+      <TabUl>
+        <TabLi
+          id={FIND_ID}
+          onClick={onTabClick}
+          clicked={isFindIdTab ? "true" : "false"}
+        >
+          <TabSpan>아이디 찾기</TabSpan>
+        </TabLi>
+        <TabLi
+          id={FIND_PW}
+          onClick={onTabClick}
+          clicked={isFindIdTab ? "false" : "true"}
+        >
+          <TabSpan>비밀번호 찾기</TabSpan>
+        </TabLi>
+      </TabUl>
 
-      <ul id="input-container">
-        <li name="name">
-          <label htmlFor="name">이름</label>
-          <input type="text" id="name" name={NAME} onChange={onChange}></input>
-        </li>
+      <ContentUl>
+        <ContentLi name="name">
+          <Label htmlFor="name">이름</Label>
+          <Input type="text" id="name" name={NAME} onChange={onChange} />
+        </ContentLi>
         {isFindIdTab ? null : (
-          <li>
-            <label htmlFor="id">아이디(이메일)</label>
-            <input type="text" id="id" name={ID} onChange={onChange}></input>
-          </li>
+          <ContentLi>
+            <Label htmlFor="id">아이디(이메일)</Label>
+            <Input type="text" id="id" name={ID} onChange={onChange} />
+          </ContentLi>
         )}
-        <li name="phone">
+        <ContentLi name="phone">
+          <Label>전화번호</Label>
           <PhoneInput
             name={[PH_FIRST, PH_MID, PH_LAST]}
             data={info}
             onChange={onChange}
           />
-        </li>
-        <li id="btn-container">
-          <button type="submit" onClick={onSubmit}>
+        </ContentLi>
+        <BtnWrapper id="btn-container">
+          <SubmitBtn type="submit" onClick={onSubmit}>
             {isFindIdTab ? "아이디 찾기" : "비밀번호 찾기"}
-          </button>
-        </li>
-      </ul>
+          </SubmitBtn>
+        </BtnWrapper>
+      </ContentUl>
       {showPopup ? (
         <div>
           <IconX onClick={onClosePopupClick} />
@@ -150,8 +162,93 @@ const FindAccountPage = () => {
           </div>
         </div>
       ) : null}
-    </div>
+    </FindAccountWrapper>
   );
 };
 
+const FindAccountWrapper = styled.div`
+  width: 590px;
+  margin: 50px auto;
+`;
+
+const TabUl = styled.ul`
+  width: 100%;
+  height: 50px;
+  margin-top: 65px;
+`;
+
+const TabLi = styled.li`
+  display: inline-block;
+  width: 49.5%;
+  height: 100%;
+  text-align: center;
+  border-radius: 7px 7px 0 0;
+  color: ${({ clicked }) => (clicked === "true" ? "#f0fbfd" : "#757575")};
+  background-color: ${({ clicked }) =>
+    clicked === "true" ? "#1c101f" : "#eeeeee"};
+
+  border-bottom: solid 1px #1c101f;
+`;
+
+const TabSpan = styled.span`
+  position: relative;
+  display: inline-block;
+  top: 50%;
+  transform: translate(0%, -50%);
+`;
+
+const ContentUl = styled.ul`
+  width: 100%;
+  height: auto;
+`;
+
+const ContentLi = styled.li`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 70px;
+`;
+
+const Label = styled.label`
+  display: inline-block;
+  width: 120px;
+  font-weight: bold;
+  padding: 10px 10px;
+  text-align: center;
+`;
+
+const Input = styled.input`
+  display: inline-block;
+  height: 60%;
+  width: ${({ width }) => (width !== undefined ? `${width}px` : "68%")};
+  border: none;
+  border-bottom: solid black 1px;
+  padding-left: 5px;
+  font-size: 14.5px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  height: 40px;
+  align-items: center;
+  margin: 20px 0 100px 0;
+`;
+
+const SubmitBtn = styled.button`
+  display: inline-block;
+  width: 100%;
+  height: 40px;
+  margin: 0px auto;
+  font-size: 15px;
+  border-radius: 5px;
+  outline: 0;
+  font-weight: bold;
+  border: none;
+  background-color: #1c101f;
+  color: #f0fbfd;
+  cursor: pointer;
+`;
 export default FindAccountPage;

d23ef16afe7a114cebf1e5dd279a6c1c3d212193
최정윤 <99jenny0614@naver.com>
2022-02-18 10:00:00 +0900

    feat: 회원가입완료페이지 css

--- src/pages/client/SignUpFinPage.jsx
+++ src/pages/client/SignUpFinPage.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 const SignUpFinPage = () => {
@@ -8,21 +9,63 @@ const SignUpFinPage = () => {
   const onClickToMain = () => navigate("/");
   const onClickToLogin = () => navigate("/login");
   return (
-    <div>
-      <div>
-        <FontAwesomeIcon icon={faCircleCheck} style={{ fontSize: 80 }} />
-      </div>
-      <p>회원가입이 완료되었습니다.</p>
-      <ul>
+    <SignUpFinPageWrapper>
+      <IconWrapper>
+        <FontAwesomeIcon icon={faCircleCheck} style={{ fontSize: 100 }} />
+      </IconWrapper>
+      <Ptag>회원가입이 완료되었습니다.</Ptag>
+      <BtnWrapper>
         <li>
-          <button onClick={onClickToMain}>메인페이지</button>
+          <Button onClick={onClickToMain}>메인페이지</Button>
         </li>
         <li>
-          <button onClick={onClickToLogin}>로그인하기</button>
+          <Button onClick={onClickToLogin}>로그인하기</Button>
         </li>
-      </ul>
-    </div>
+      </BtnWrapper>
+    </SignUpFinPageWrapper>
   );
 };
+
+const SignUpFinPageWrapper = styled.div`
+  display: flex;
+  width: 590px;
+  margin: 60px auto;
+  height: 380px;
+  border-top: 1.4px solid black;
+  border-bottom: 1.4px solid black;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Ptag = styled.p`
+  margin-top: 20px;
+`;
+
+const IconWrapper = styled.div`
+  margin: 55px 0 30px 0;
+`;
+
+const BtnWrapper = styled.ul`
+  display: flex;
+  margin: 30px 0px 40px 0px;
+`;
+
+const Button = styled.button`
+  height: 40px;
+  width: 100px;
+  background-color: white;
+  color: #1c101f;
+  border: 1px solid #1c101f;
+  outline: 0;
+  border-radius: 6px;
+  margin: 0 3px;
+  font-weight: bold;
+
+  &:hover {
+    background-color: #1c101f;
+    color: #f0fbfd;
+  }
+`;
 
 export default SignUpFinPage;

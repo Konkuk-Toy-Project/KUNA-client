c606efa950c3413f126ee0719ce1a0d72172c90b
최정윤 <99jenny0614@naver.com>
2022-02-20 12:00:38 +0900

    feat: 리뷰 컴포넌트 css

--- src/components/client/Review/Review.jsx
+++ src/components/client/Review/Review.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
-import RatingStars from "./RatingStars";
-import UserName from "./UserName";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const Review = ({ reviewObj, setPopup }) => {
+import styled from "styled-components";
+import ReviewHeader from "./ReviewHeader";
+import { useSetRecoilState } from "recoil";
+import { reviewPopupState } from "../../../store/client/popup";
+
+const Review = ({ reviewObj }) => {
   const [data, setData] = useState(reviewObj);
   const [textBoxType, setTextBoxType] = useState("hidden");
-
+  const setPopup = useSetRecoilState(reviewPopupState);
   const onImgClick = (e) => {
     setPopup({ ...data, ["selImgIdx"]: parseInt(e.target.name) }); // 선택된 아이템 정보 입력
   };
@@ -20,45 +23,67 @@ const Review = ({ reviewObj, setPopup }) => {
   // rate: 별점
   // registryDate: 리뷰 등록 일시
   // reviewImagesUrl(array): 리뷰 이미지 저장 이름
+  //console.log(popup);
 
   return (
-    <div>
-      <div name="header">
-        <UserName name={data.memberName} />
-        <div>{data.option}</div>
-        <div name="user-rate">
-          <RatingStars rate={data.rate} />
-          <span>{data.rate.toFixed(1)}</span>
+    <ReviewWrapper>
+      <ReviewInnerWrapper>
+        <ReviewHeader
+          rate={data.rate}
+          memberName={data.memberName}
+          option={data.option}
+          date={data.registryDate}
+        />
+
+        <div name="container">
+          {data.reviewImagesUrl.length === 0 ? null : (
+            <ReviewImgWrapper name="imgs">
+              {data.reviewImagesUrl.map((img, key) => (
+                <ReviewImg
+                  name={key}
+                  key={key}
+                  src={`http://localhost:8080/image/review/${img}`}
+                  onClick={onImgClick}
+                  width={100}
+                  height={100}
+                  overflow="hidden"
+                />
+              ))}
+            </ReviewImgWrapper>
+          )}
+          <div name="text" style={{ overflow: { textBoxType } }}>
+            <p style={{ whiteSpace: "pre-line" }} onClick={onTextClick}>
+              {data.description}
+            </p>
+            {/* <button onClick={onTextClick}>리뷰 더보기</button> */}
+          </div>
         </div>
-      </div>
-      <div name="container">
-        <div name="text" style={{ overflow: { textBoxType } }}>
-          <p style={{ whiteSpace: "pre-line" }} onClick={onTextClick}>
-            {data.description}
-          </p>
-          {/* <button onClick={onTextClick}>리뷰 더보기</button> */}
-        </div>
-        <div name="imgs">
-          {data.reviewImagesUrl.map((img, key) => (
-            <img
-              name={key}
-              key={key}
-              src={`http://localhost:8080/image/review/${img}`}
-              onClick={onImgClick}
-              width={100}
-              height={100}
-              overflow="hidden"
-            />
-          ))}
-        </div>
-        <div>{data.registryDate}</div>
-      </div>
-    </div>
+      </ReviewInnerWrapper>
+    </ReviewWrapper>
   );
 };
 
 Review.propTypes = {
   reviewObj: PropTypes.object.isRequired,
-  setPopup: PropTypes.func.isRequired,
 };
+
+const ReviewWrapper = styled.div`
+  width: 100%;
+  padding: 15px 0 35px 0;
+  border-bottom: solid 1px #cfcfcf;
+`;
+const ReviewInnerWrapper = styled.div`
+  margin: 0 20px;
+`;
+
+const ReviewImgWrapper = styled.div`
+  margin: 10px 0 20px 0;
+`;
+const ReviewImg = styled.img`
+  height: 100px;
+  width: 100px;
+  border-radius: 5px;
+  margin: 0 2px;
+  object-fit: cover;
+`;
 export default Review;

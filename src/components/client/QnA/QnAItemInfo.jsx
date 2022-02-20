0db0671f226577f1c17a4d4894e2e8500b7d0bc8
최정윤 <99jenny0614@naver.com>
2022-02-20 17:53:19 +0900

    feat: qna 팝업에서 아이템 정보 컴포넌트 css

--- src/components/client/QnA/QnAItemInfo.jsx
+++ src/components/client/QnA/QnAItemInfo.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 const QnAItemInfo = ({ thumbnail, name }) => {
   return (
     <div>
-      <div name="productInfoSection">
-        <img name="thumbnail" src={thumbnail} alt="썸네일" />
-        <div name="상품명">{name}</div>
-      </div>
+      <InfoWrapper name="productInfoSection">
+        <Img name="thumbnail" src={thumbnail} alt="썸네일" />
+        <ItemName name="상품명">{name}</ItemName>
+      </InfoWrapper>
     </div>
   );
 };
@@ -16,5 +17,24 @@ QnAItemInfo.propTypes = {
   thumbnail: PropTypes.string,
   name: PropTypes.string.isRequired,
 };
+
+const InfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Img = styled.img`
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+`;
+
+const ItemName = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  text-align: left;
+  padding: 0px 10px;
+`;
 
 export default QnAItemInfo;

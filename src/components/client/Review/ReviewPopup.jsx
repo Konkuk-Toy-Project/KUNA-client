da396064f8c1b82e354c21850f2ec309095b398d
최정윤 <99jenny0614@naver.com>
2022-02-20 12:00:52 +0900

    feat: 리뷰 팝업 컴포넌트 css

--- src/components/client/Review/ReviewPopup.jsx
+++ src/components/client/Review/ReviewPopup.jsx
@@ -1,45 +1,104 @@
 import React from "react";
-import PropTypes from "prop-types";
 import ImgSlide from "../../common/ImgSlide/ImgSlide";
-import UserName from "./UserName";
-import RatingStars from "./RatingStars";
 import IconX from "../Icon/IconX";
+import ReviewHeader from "./ReviewHeader";
+import styled from "styled-components";
+import { useRecoilState } from "recoil";
+import { reviewPopupState } from "../../../store/client/popup";
 
-const ReviewPopup = ({ data, setData }) => {
+const ReviewPopup = () => {
   const onClick = () => setData({});
+  const [data, setData] = useRecoilState(reviewPopupState);
 
   return (
-    <div>
+    <PopupBackGround>
       {Object.keys(data).length === 0 ? null : (
-        <div>
-          <button onClick={onClick}>
-            <IconX />
-          </button>
-          <ImgSlide
-            imgsrcs={data.reviewImagesUrl.map(
-              (src) => `http://localhost:8080/image/review/${src}`
-            )}
-            defaultIdx={data.selImgIdx}
-          />
+        <PopupContentWrapper>
+          <IconXWrapper>
+            <IconX onClick={onClick} />
+          </IconXWrapper>
+          <ImgSlideWrapper>
+            <ImgSlide
+              imgsrcs={data.reviewImagesUrl.map(
+                (src) => `http://localhost:8080/image/review/${src}`
+              )}
+              defaultIdx={data.selImgIdx}
+              mainW={"410px"}
+              subW={"80px"}
+            />
+          </ImgSlideWrapper>
 
-          <div name="textSection" backgroundcolor="#f3e5f5">
-            <UserName name={data.memberName} />
-            <div>{data.option}</div>
-            <RatingStars rate={data.rate} />
-            <span>{data.rate}</span>
-            <br />
-            <div name="text">{data.description}</div>
-            <div>{data.registryDate}</div>
-          </div>
-        </div>
+          <TextWrapper>
+            <HeaderWrapper>
+              <ReviewHeader
+                rate={data.rate}
+                memberName={data.memberName}
+                option={data.option}
+                date={data.registryDate}
+              />
+            </HeaderWrapper>
+
+            <DescriptWrapper>{data.description}</DescriptWrapper>
+          </TextWrapper>
+        </PopupContentWrapper>
       )}
-    </div>
+    </PopupBackGround>
   );
 };
 
-ReviewPopup.propTypes = {
-  data: PropTypes.object.isRequired,
-  setData: PropTypes.func.isRequired,
-};
+const PopupBackGround = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 1;
+`;
+const PopupContentWrapper = styled.div`
+  width: 850px;
+  margin: 30px 0;
+  display: flex;
+  background-color: white;
+  justify-content: space-around;
+  border-radius: 5px;
+  position: relative;
+`;
+
+const IconXWrapper = styled.div`
+  text-align: right;
+  font-size: 20px;
+  z-index: 2;
+  position: absolute;
+  top: 1%;
+  right: 1%;
+`;
+
+const TextWrapper = styled.div`
+  width: 45%;
+  background-color: white;
+  border-radius: 5px;
+  margin: 5px;
+`;
+
+const HeaderWrapper = styled.div`
+  box-sizing: border-box;
+  margin: 30px 0;
+  padding: 0 0 15px 0;
+  border-bottom: 1px #bdbdbd solid;
+`;
+
+const DescriptWrapper = styled.div`
+  line-height: 180%;
+  padding: 0 10px;
+`;
+
+const ImgSlideWrapper = styled.div`
+  margin: 10px 5px;
+  width: 45%;
+`;
 
 export default ReviewPopup;

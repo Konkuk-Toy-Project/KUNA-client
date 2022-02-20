0cf2fd268ea0b6cfc2c022b8bb2ec50a82a942b5
최정윤 <99jenny0614@naver.com>
2022-02-21 01:40:42 +0900

    refactor: 아이템 수량조절 -  updown 컴포넌트로 교체

--- src/components/client/Order/OrderedItem.jsx
+++ src/components/client/Order/OrderedItem.jsx
@@ -3,41 +3,37 @@ import PropTypes from "prop-types";
 import IconX from "../Icon/IconX";
 import { useRecoilState } from "recoil";
 import { buyingState } from "../../../store/client/buying";
-
-const UP = "up";
-const DOWN = "down";
+import UpDownBtn from "../../common/UpDownBtn/UpDownBtn";
+import styled from "styled-components";
 
 const OrderedItem = ({ item }) => {
   const [buying, setBuying] = useRecoilState(buyingState);
 
-  const onClick = (e) => {
-    switch (e.target.name) {
-      case UP:
-        item.count < item.stock
-          ? setBuying(
-              buying.map((i) =>
-                i.itemId === item.itemId &&
-                i.option1Id === item.option1Id &&
-                i.option2Id === item.option2Id
-                  ? { ...i, count: i.count + 1 }
-                  : i
-              )
-            )
-          : alert("수량 초과입니다.");
+  const onUpBtnClick = (e) => {
+    item.count < item.stock
+      ? setBuying(
+          buying.map((i) =>
+            i.itemId === item.itemId &&
+            i.option1Id === item.option1Id &&
+            i.option2Id === item.option2Id
+              ? { ...i, count: i.count + 1 }
+              : i
+          )
+        )
+      : alert("수량 초과입니다.");
+  };
 
-        break;
-      case DOWN:
-        if (item.count > 1) {
-          setBuying(
-            buying.map((i) =>
-              i.itemId === item.itemId &&
-              i.option1Id === item.option1Id &&
-              i.option2Id === item.option2Id
-                ? { ...i, count: i.count - 1 }
-                : i
-            )
-          );
-        }
+  const onDownBtnClick = (e) => {
+    if (item.count > 1) {
+      setBuying(
+        buying.map((i) =>
+          i.itemId === item.itemId &&
+          i.option1Id === item.option1Id &&
+          i.option2Id === item.option2Id
+            ? { ...i, count: i.count - 1 }
+            : i
+        )
+      );
     }
   };
 
@@ -60,54 +56,118 @@ const OrderedItem = ({ item }) => {
 
   console.log(buying);
   return (
-    <div>
-      <div>{item.name}</div>
-      <div>
-        <img
-          src={`http://localhost:8080/image/thumbnail/${item.thumbnail}`}
+    <OrderedItemWrapper>
+      <ThumbnailWrapper>
+        <Thumbnail
+          src={`http://localhost:8080/image/thumbnail/${item.thumbnailImg}`}
           alt="사진없음"
         />
-        <div>
+      </ThumbnailWrapper>
+
+      <NameWrapper>
+        <ItemName>{item.name}</ItemName>
+        <OptionName>
           {item.option2 === ""
             ? item.option1
             : item.option1 + ", " + item.option2}
-        </div>
+        </OptionName>
+      </NameWrapper>
 
-        {item.sale > 0 ? (
-          <div name="price-wrapper">
-            <span
-              name="price"
-              style={{ display: "block", textDecoration: "line-through" }}
-            >
-              {item.price}원
-            </span>
-            <span name="sale">{item.sale}%</span>
-            <span name="sale-price">
-              {(item.price * (100 - item.sale)) / 100}원
-            </span>
-          </div>
-        ) : (
-          <div id="non-sale">{item.price}원</div>
-        )}
+      {item.sale > 0 ? (
+        <PriceWrapper name="price-wrapper">
+          <span
+            name="price"
+            style={{ display: "block", textDecoration: "line-through" }}
+          >
+            {item.price.toLocaleString()}원
+          </span>
+          <span name="sale-price">
+            {((item.price * (100 - item.sale)) / 100).toLocaleString()}원
+          </span>
+        </PriceWrapper>
+      ) : (
+        <PriceWrapper id="non-sale">
+          {item.price.toLocaleString()}원
+        </PriceWrapper>
+      )}
 
-        <div>
-          <label>수량</label>
+      <CounterWrapper>
+        <CounterBox>
           <span>{item.count}</span>
-          <div>
-            <button name={UP} onClick={onClick}>
-              🔼
-            </button>
-            <button name={DOWN} onClick={onClick}>
-              🔽
-            </button>
-          </div>
-        </div>
-      </div>
-      <IconX onClick={onDelClick} />
-    </div>
+        </CounterBox>
+        <UpDownBtn
+          onUpBtnClick={onUpBtnClick}
+          onDownBtnClick={onDownBtnClick}
+          width={"50px"}
+        />
+      </CounterWrapper>
+      <IconXWrapper>
+        <IconX onClick={onDelClick} />
+      </IconXWrapper>
+    </OrderedItemWrapper>
   );
 };
 
 OrderedItem.propTypes = {};
+
+const OrderedItemWrapper = styled.div`
+  width: 100%;
+  padding: 10px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: solid 0.5px #9e9e9e;
+`;
+
+const ThumbnailWrapper = styled.div`
+  flex-basis: 20%;
+  text-align: center;
+`;
+
+const Thumbnail = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const NameWrapper = styled.div`
+  flex-basis: 35%;
+`;
+
+const ItemName = styled.div`
+  font-weight: bold;
+  margin: 3px 0px;
+`;
+
+const OptionName = styled.div`
+  font-size: 13px;
+  color: #707070;
+  padding: 10px 0px;
+`;
+
+const PriceWrapper = styled.div`
+  flex-basis: 15%;
+  text-align: center;
+`;
+const CounterWrapper = styled.div`
+  flex-basis: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CounterBox = styled.div`
+  width: 30%;
+  height: 35px;
+  border-radius: 5px;
+  border: solid 1px #bcbcbc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IconXWrapper = styled.div`
+  flex-basis: 10%;
+  text-align: center;
+`;
 
 export default OrderedItem;

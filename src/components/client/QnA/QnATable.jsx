0a839fe82827854f089459b79f6f4523cb74284e
최정윤 <99jenny0614@naver.com>
2022-02-20 14:25:53 +0900

    feat: qnA table css

--- src/components/client/QnA/QnATable.jsx
+++ src/components/client/QnA/QnATable.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import PageChanger from "../../common/PageChanger/PageChanger";
+import styled from "styled-components";
 
 const QNA_NUM_UNIT = 5;
 
@@ -11,35 +12,39 @@ const QnATable = ({ qnAs, setSelAnswIdx }) => {
   const onAnswClick = (e) => setSelAnswIdx(e.target.dataset.id);
 
   return (
-    <div>
-      <table>
+    <QnATableWrapper>
+      <Table>
         <colgroup>
-          <col />
-          <col />
-          <col />
-          <col />
-          <col />
+          <col style={{ width: "10%" }} />
+          <col style={{ width: "45%" }} />
+          <col style={{ width: "20%" }} />
+          <col style={{ width: "12.5%" }} />
+          <col style={{ width: "12.5%" }} />
         </colgroup>
-        <thead>
+        <Thead>
           <tr>
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>날짜</th>
-            <th>답변상태</th>
+            <Th>번호</Th>
+            <Th>제목</Th>
+            <Th>작성자</Th>
+            <Th>날짜</Th>
+            <Th>답변상태</Th>
           </tr>
-        </thead>
+        </Thead>
         <tbody>
           {qnAs.map((q, idx) =>
             idx + 1 >= (curPageNum - 1) * QNA_NUM_UNIT + 1 &&
             idx + 1 <= curPageNum * QNA_NUM_UNIT ? (
-              <tr key={"qna_" + idx}>
+              <Tr key={"qna_" + idx}>
                 <td>{idx + 1}</td>
                 <td>{q.secret == true ? "비밀글입니다." : q.title}</td>
                 <td>{q.memberName}</td>
-                <td>{q.registryDate === null ? "" : q.registryDate}</td>
                 <td>
-                  <button
+                  {q.registryDate === null
+                    ? "-"
+                    : q.registryDate.substring(0, 10)}
+                </td>
+                <td>
+                  <AnswButton
                     onClick={onAnswClick}
                     disabled={q.title === null || !q.answered}
                     data-id={idx}
@@ -49,13 +54,13 @@ const QnATable = ({ qnAs, setSelAnswIdx }) => {
                       : q.title != null
                       ? "답변확인"
                       : "답변완료"}
-                  </button>
+                  </AnswButton>
                 </td>
-              </tr>
+              </Tr>
             ) : null
           )}
         </tbody>
-      </table>
+      </Table>
 
       <PageChanger
         data={qnAs}
@@ -65,10 +70,52 @@ const QnATable = ({ qnAs, setSelAnswIdx }) => {
         setTotalPageNum={setTotalPageNum}
         pageUnit={5}
       />
-    </div>
+    </QnATableWrapper>
   );
 };
 
 QnATable.propTypes = { qnAs: PropTypes.array.isRequired };
+const QnATableWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Table = styled.table`
+  text-align: center;
+`;
+const Thead = styled.thead`
+  border-top: solid black 2px;
+  border-bottom: solid #8d8d8d 1px;
+  height: 50px;
+  line-height: 50px;
+`;
+const Th = styled.th`
+  font-weight: bold;
+`;
+
+const Tr = styled.tr`
+  height: 50px;
+  line-height: 50px;
+  border-bottom: 0.5px solid #efefef;
+`;
+
+const AnswButton = styled.button`
+  display: inline-block;
+  height: 27px;
+  widht: 120px;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+
+  background-color: #ab47bc;
+  color: #f5f5f5;
+
+  &:disabled {
+    background-color: #cfcfcf;
+    color: #9e9e9e;
+  }
+`;
 
 export default QnATable;

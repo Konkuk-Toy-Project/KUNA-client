7e0c9fd6c3b6b8c4723161193a787a715ca17d6c
최정윤 <99jenny0614@naver.com>
2022-02-20 11:55:43 +0900

    feat: 아이템 detail 컴포넌트 css

--- src/components/client/ItemDetail/ItemDetailImg.jsx
+++ src/components/client/ItemDetail/ItemDetailImg.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 const ItemDetailImg = ({ imgSrc }) => {
   return (
-    <div>
+    <ImgWrapper>
       {imgSrc.map((src, idx) => (
-        <img
+        <Img
           key={"img_" + idx}
           src={`http://localhost:8080/image/detail/${src}`}
           alt="상품 상세 정보"
         />
       ))}
-    </div>
+    </ImgWrapper>
   );
 };
+
+const ImgWrapper = styled.div`
+  width: 80%;
+  margin: 0px auto;
+`;
+const Img = styled.img`
+  display: inline-block;
+  width: 100%;
+`;
 
 export default ItemDetailImg;
